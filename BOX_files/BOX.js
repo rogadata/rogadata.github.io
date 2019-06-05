609c6522ab25321b7ccd32e0911cf5e764b28a28
rogadata <46431522+rogadata@users.noreply.github.com>
2019-06-05 18:06:28 -0400

    Delete BOX.js

--- BOX_files/BOX.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20190605
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('BOX_files/BOXMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
